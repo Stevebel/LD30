ee8a1420296bf2bc61e5c27bd7fe8fb2126e3119
Steven B <stevebel6@gmail.com>
2014-08-22 22:34:11 -0400

    Added Cooldown Bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour {
+    public float percent = 1.0f;
+    public GameObject inside;
+    private Transform insideTransform;
+    // Use this for initialization
+	void Start () {
+        if (inside != null)
+        {
+            insideTransform = inside.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (insideTransform != null)
+        {
+            insideTransform.localScale = new Vector3(percent, 1f, 1f);
+        }
+	}
+}
