7425a0778149150d039fcec9af21f16366c6c158
Steven B <stevebel6@gmail.com>
2014-08-22 23:31:41 -0400

    Add Harpoon Gun

---
+++ Assets/Scripts/HarpoonGun.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarpoonGun : MonoBehaviour {
+    public ProgressBar cooldownBar;
+    [Range(1f,60f)]public float cooldownSecs;
+    public LayerMask targetableLayer;
+    public Vector2 aim = new Vector2(0,0);
+    private float cooldownRemaining;
+    private GameObject[] targetable;
+    private Transform _transform;
+	// Use this for initialization
+	void Start () {
+        Shoot();
+        _transform = transform;
+    }
+
+    public void Shoot()
+    {
+        cooldownRemaining = cooldownSecs;
+
+        RaycastHit2D targetHit = Physics2D.Raycast(new Vector2(_transform.position.x, _transform.position.y), aim, 1000f, targetableLayer);
+        if (targetHit.collider != null)
+        {
+            GameObject targetObject = targetHit.collider.gameObject;
+            HarpoonTarget target = targetObject.GetComponent<HarpoonTarget>();
+            if (target != null)
+            {
+                target.Hit(this);
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+            if (cooldownRemaining < 0)
+            {
+                cooldownRemaining = 0;
+            }
+        }
+        
+        float cooldownPercent = (cooldownSecs - cooldownRemaining) / cooldownSecs;
+
+        cooldownBar.percent = cooldownPercent;
+	}
+}
---
+++ Assets/Scripts/HarpoonTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarpoonTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Hit(HarpoonGun harpoon)
+    {
+        Debug.Log("Harpoon Has Hit");
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
