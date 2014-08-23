f24d84ad62722bdcb5dbe277ac77eb99c0ae7df7
apophis42 <apophis42@gmail.com>
2014-08-22 23:14:01 -0400

    Added naive target planet control
    Sorta springy motion.

---
+++ Assets/Scripts/TargetPlanet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPlanet : MonoBehaviour
+{
+	[SerializeField] private Transform hooker;
+	[SerializeField] float acceleration;
+	[SerializeField] float maxSpeed;
+
+	void FixedUpdate()
+	{
+		if(hooker != null)
+		{
+			Vector2 direction = hooker.position - transform.position;
+			float distance = direction.magnitude;
+			direction.Normalize ();
+			rigidbody2D.AddForce (direction * acceleration * distance);
+			Vector2 velocity = rigidbody2D.velocity;
+			velocity.x = Mathf.Clamp (velocity.x, -maxSpeed, maxSpeed);
+			velocity.y = Mathf.Clamp (velocity.y, -maxSpeed, maxSpeed);
+			rigidbody2D.velocity = velocity;
+		}
+	}
+}
