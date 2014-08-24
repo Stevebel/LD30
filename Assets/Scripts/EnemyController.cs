a0e9c643131ebc175af964584ddfdd9fce399df3
apophis42 <apophis42@gmail.com>
2014-08-24 04:27:24 -0400

    Basic enemy scripting stuff
    TODO: Make actual enemy object, make laser object, change collision to
    start explosion.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	[SerializeField] float speed;
+	[SerializeField] Rigidbody2D enemyLaserPrefab;
+	[SerializeField] float laserSpeed;
+	[SerializeField] int maxHealth;
+
+	private int currentHealth;
+
+	void Awake()
+	{
+		currentHealth = maxHealth;
+	}
+
+	void Move(Vector2 direction)
+	{
+		rigidbody2D.velocity = direction.normalized * speed;
+	}
+
+	void Shoot()
+	{
+		Rigidbody2D laser = Instantiate(enemyLaserPrefab, transform.position, transform.rotation) as Rigidbody2D;
+		laser.velocity = laser.transform.forward * laserSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag == "Harpoon")
+			currentHealth--;
+
+		if(currentHealth <= 0)
+			Destroy(gameObject);
+	}
+}
