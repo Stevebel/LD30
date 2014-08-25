def59a301694bf4c5dc95805800cbdb876e5b1a0
Steven B <stevebel6@gmail.com>
2014-08-25 19:51:54 -0400

    I got a rock

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour
+{
+	[SerializeField] int maxHealth;
+	[SerializeField] GameObject explosionPrefab;
+
+	private int currentHealth;
+
+	void Awake()
+	{
+		currentHealth = maxHealth;
+        rigidbody2D.rotation = Random.Range(0, 359);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.gameObject.tag == "Harpoon")
+        {
+            currentHealth--;
+            Destroy(collision.gameObject);
+        }
+
+		if(currentHealth <= 0)
+		{
+			Instantiate (explosionPrefab, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
